c7d013cdc826cfb51f42538196aca6d596c10dd4
lpq <10086@qq.com>
2019-11-29 20:21:56 +0800

    tijiao

--- src/main/java/com/qf/asd/asds.java
+++ src/main/java/com/qf/asd/asds.java
@@ -3,10 +3,6 @@ package com.qf.asd;
 public class asds {
     public static void main(String[] args) {
         System.out.println("asdfgh");
-        System.out.println("asdfgh");
-        System.out.println("asdfgh");
 
-        System.out.println("asdfgh");
-        System.out.println("asdfgh");
     }
 }
